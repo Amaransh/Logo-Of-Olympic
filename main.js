eee9adec3f0a8df9dcf194606aa99138d5af3e49
Amaransh <90545826+Amaransh@users.noreply.github.com>
2022-01-12 15:42:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,161 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 10;
+ctx.rect(150,155,430,430);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(250 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(300 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(350 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(400 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(450 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(500 , 200 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 250 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 300 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 350 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 400 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 450 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(500 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(500 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(450 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(400 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(350 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(300 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 10;
+ctx.arc(250 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 500 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 450 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 400 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 350 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 300 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 10;
+ctx.arc(250 , 250 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 10;
+ctx.arc(375 , 375 , 40 , 0 , 2* Math.PI);
+ctx.stroke()
